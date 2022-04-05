8e62c48a029f50cba3364bc5f2b87820cf5c6abc
MacBobby Chibuzor <theghostmac@gmail.com>
2022-04-05 06:25:23 +0100

    solidity counter contract

---
+++ SolidityByExampleReview/counter.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// Compiler version must be in the range:
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint public count;
+
+    function getCurrentCount() public view returns () {
+        return count;
+    }
+
+    function incrementCountby1() public {
+        count += 1;
+    }
+
+    function decrementCountby1() public {
+        count -= 1;
+    }
+}
