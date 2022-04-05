ff8f54c60a0067062848ae53c154af9bb249ab42
MacBobby Chibuzor <theghostmac@gmail.com>
2022-04-05 12:17:05 +0100

    messenger contract

---
+++ PluralsightReview/storer.sol
@@ -0,0 +1,10 @@
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.4.0;
+
+// contract HelloWorld {
+//     string public message;
+
+//     function setMessage(string newMessage) public {
+//         message = newMessage;
+//     }
+// }
