1564517698a04c8ba6336e8e8f0373e7842c7395
MacBobby Chibuzor <theghostmac@gmail.com>
2022-04-05 14:32:02 +0100

    control structures

--- SolidityByExampleReview/statevar.sol
+++ SolidityByExampleReview/statevar.sol
@@ -12,4 +12,28 @@ contract SimpleStorage {
     function get() public view returns (uint) {
         return num;
     }
+
+    // ether and wei
+    /* transactions are paid with ether. 1 ether = 10**18 wei.
+
+    */
+    uint public oneWei = 1 wei;
+    bool public isOneWei = 1 wei == 1;
+
+    uint public oneEther = 1 ether;
+    bool public isOneEther = 1 ether == 1e18;
+
+    // gas and gas price
+    // ether = gas spent * gas price
+    // gas bounds
+    // gas limit and block gas limit
+
+    uint public i = 0;
+    // forever() runs a loop until all gas finishes and transaction fails.
+    function forever() public {
+        while(true) {
+            i += 1;
+        }
+    }
+
 }
