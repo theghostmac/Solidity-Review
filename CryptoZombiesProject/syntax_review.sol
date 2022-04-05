e51b5a04add7ac69c8bd255723378f69fca5d67c
MacBobby Chibuzor <theghostmac@gmail.com>
2022-04-05 06:15:40 +0100

    zombiefactory contract created

---
+++ CryptoZombiesProject/syntax_review.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+/*
+ Compiler version must be greater than or 
+ equal to 0.8.0 and less than 0.9.0
+*/
+pragma solidity ^0.8.0;
+
+contract ZombieFactory {
+    
+}
