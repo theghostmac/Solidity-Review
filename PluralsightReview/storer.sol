f134a1621fdbca08efa9dd0e7aeda3859e9c357b
MacBobby Chibuzor <theghostmac@gmail.com>
2022-04-05 12:17:54 +0100

    storer.sol file refactored

--- PluralsightReview/storer.sol
+++ PluralsightReview/storer.sol
@@ -1,5 +1,6 @@
 // // SPDX-License-Identifier: MIT
-// pragma solidity ^0.4.0;
+// change compiler version to 0.4.0 to run this
+pragma solidity ^0.8.0;
 
 // contract HelloWorld {
 //     string public message;
